320ce1f2a9367472983ada3bce06561f6e1b7659
Leonardo Lopez <leodanlors1993@gmail.com>
2019-11-11 18:27:44 -0600

    cubox, js y SASS

---
+++ script.js
@@ -0,0 +1,20 @@
+let showOverlay = (action) => {
+        let overlay = document.querySelector(".overlay")
+        overlay.style.display = "grid"
+        
+
+}
+
+let exitOverlay = (action) => {
+    let overlay = document.querySelector(".overlay")
+    overlay.style.display = "none"
+}
+
+
+let obtenerDatos = () => {
+    let email = document.querySelector(".form__datos--email").value
+    let nombre = document.querySelector(".form__datos--nombre").value
+    let telefono = document.querySelector(".form__datos--tel").value
+
+    
+}
